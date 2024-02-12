3ea042f29f2cbf08737fd31f0310ffe3760f628e
Takumi Miyawaki <57098509+tm2904@users.noreply.github.com>
2024-02-12 11:59:51 +0900

    Create serve.py

---
+++ serve.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+import socket
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/',methods = ['POST', 'GET'])
+def seedNumber():
+   if request.method == 'GET':
+      return socket.gethostname()
+   elif request.method == 'POST':
+        cmd = ['python', 'stress_cpu.py']
+        proc = subprocess.Popen(cmd)
+        return 'posted'
+   else:
+      return 'Not supported'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
